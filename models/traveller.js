fd3cd59d492efd4f439648986cb74cb835576857
michaeld55 <michaeld55@hotmail.co.uk>
2020-03-25 17:23:23 +0000

    finiished hw

---
+++ models/traveller.js
@@ -0,0 +1,36 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  result = this.journeys.map(journey => journey.startLocation);
+  return result
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  result = this.journeys.map(journey => journey.endLocation);
+  return result;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  result = this.journeys.filter(journey => journey.transport === transport);
+  return result;
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  result = this.journeys.filter(journey => journey.distance > minDistance);
+  return result;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  result = this.journeys.reduce((total, journey) => journey.distance += total, 0);
+  return result;
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const unique = [...new Set(this.journeys.map(journey => journey.transport))];
+  return unique
+};
+
+
+module.exports = Traveller;
